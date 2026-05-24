a8c12d12f94371e785fc462f1e2a95fb6119447a
vensoeng <likakonpouw@gmail.com>
2026-05-24 21:25:59 +0700

    update allow route on project

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const path = require('path');
 const app = express();
 
 const allowedOrigins = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
+    // 'http://localhost:3000',
+    // 'http://127.0.0.1:3000',
+    // 'http://localhost:5173',
+    // 'http://127.0.0.1:5173',
     'http://vensoeng.vercel.app',
     'http://vensoeng.free.nf',
     process.env.FRONTEND_URL
